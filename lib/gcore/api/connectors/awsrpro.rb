775954d8c69d4adaa3efe4c9e996de481761bc6b
Ardee Aram <ardee.aram@galoretv.com>
2014-12-16 14:07:17 +0800

    Added Gcore::Api::Stores::SalesOrders.sku_count

--- lib/gcore/api/connectors/awsrpro.rb
+++ lib/gcore/api/connectors/awsrpro.rb
@@ -8,9 +8,18 @@ module Gcore
          def self.list(params = {})
           #Backwards compatible with store_id,
           #But most appropriate with store_code         
-          store_code = params[:store_id] || params[:store_code] 
+          store_code = params[:store_id] || params[:store_code]
+          with_meta = params[:with_meta]
+           
           url = "#{Gcore::Api.endpoint}/connectors/awsrpro/#{store_code}"
-          JSON.parse(RestClient.get(url, Gcore::Api.header))           
+          
+          if (with_meta === true)
+            response = RestClient.get(url, Gcore::Api.header)
+            return [JSON.parse(response), response.headers]
+          else
+            return JSON.parse(RestClient.get(url, Gcore::Api.header))
+          end
+                     
          end      
       end
     end
--- lib/gcore/api/stores/sales_orders.rb
+++ lib/gcore/api/stores/sales_orders.rb
@@ -5,6 +5,7 @@ module Gcore
   class Api
     class Stores
       class SalesOrders
+        
         def self.list(params = {})
           store_code = params[:store_id] || params[:store_code]
           allowed_params = [:limit, :page]
@@ -12,6 +13,14 @@ module Gcore
           url = "#{Gcore::Api.endpoint}/stores/#{store_code}/sales_orders"
           JSON.parse(RestClient.get(url, Gcore::Api.header.merge(params: url_params)))                
         end
+        
+        def self.sku_count(params = {})
+          store_code = params[:store_id] || params[:store_code]
+          sku = params[:sku]
+          url = "#{Gcore::Api.endpoint}/stores/#{store_code}/sales_orders/sku_count/#{sku}"
+          JSON.parse(RestClient.get(url, Gcore::Api.header))  
+        end
+        
       end
     end
   end
