27c6862079b9feb3da4a3ae29e32d2a200492194
malbarando <malbarando@gmail.com>
2015-03-19 03:09:04 +0800

    Modified awstiger connector, added SalesOrders.list_items_summary()

--- lib/gcore/api/connectors/awstiger.rb
+++ lib/gcore/api/connectors/awstiger.rb
@@ -11,13 +11,29 @@ module Gcore
           store_code = params[:store_id] || params[:store_code]
           with_meta = params[:with_meta]
 
-          url = "#{Gcore::Api.endpoint}/connectors/awstiger/#{store_code}"
+          url = "#{Gcore::Api.endpoint}/connectors/awstiger/#{store_code}/async"
+          resp = RestClient.post(url, {}, Gcore::Api.header)
+          response_location = resp.headers[:location]
+          response_url = "#{Gcore::Api.endpoint}#{response_location}"
+          output = ""
+
+          loop do
+
+            repeat_request = false
+            begin
+              output = RestClient.get(response_url, Gcore::Api.header)
+            rescue RestClient::Exception => ex
+              repeat_request = true
+              sleep(0.25) #Do not overwhelm the server
+            end
+
+            break if !repeat_request
+          end
 
           if (with_meta === true)
-            response = RestClient.get(url, Gcore::Api.header)
-            return [JSON.parse(response, {symbolize_names: true}), response.headers]
+            return [JSON.parse(output, {symbolize_names: true}), output.headers]
           else
-            return JSON.parse(RestClient.get(url, Gcore::Api.header), {symbolize_names: true})
+            return JSON.parse(output, {symbolize_names: true})
           end
 
         end
