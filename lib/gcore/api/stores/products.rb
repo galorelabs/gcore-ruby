e9fb4beadd2bd060a2fd30949cbe7be156a20d5f
Ardee Aram (Codebox) <ardee.aram@galoretv.com>
2015-03-20 09:17:20 +0800

    Gcore::Api::Stores::Products.mass_update_quantity

--- lib/gcore/api/stores/products.rb
+++ lib/gcore/api/stores/products.rb
@@ -1,3 +1,4 @@
+require 'gcore/api'
 require 'json'
 require 'rest-client'
 require 'base64'
@@ -68,6 +69,46 @@ module Gcore
              url = "#{Gcore::Api.endpoint}/stores/#{store_code}/products/skus"
              JSON.parse(RestClient.get(url, Gcore::Api.header), {symbolize_names: true})
         end
+
+        # PUT /stores/cudsly/products/quantity        
+        def self.mass_update_quantity(params = {})
+            store_code = params[:store_id] || params[:store_code]
+            delta = params[:delta]
+            verbose_mode = params[:verbose_mode]
+            body = params[:body]
+            url = "#{Gcore::Api.endpoint}/stores/#{store_code}/products/quantity"
+            url += "?delta=true" if delta
+            Gcore::Api.logger.puts(url) if verbose_mode
+
+            #We are concerned with the header only
+            resp = RestClient.put(url, body.to_json, Gcore::Api.header).headers
+            output = resp.select{|key, value| [:location, :x_batch].include? key}
+
+            #Poll the result
+
+            loop do
+                begin 
+                    #Use HEAD for faster polling instead of GET
+                    results = RestClient.head("#{Gcore::Api.endpoint}#{output[:location]}", Gcore::Api.header).headers
+                    results.select!{|key, value| [:x_object_processed, :x_object_count].include? key}
+                    results[:percentage] = ((BigDecimal.new(results[:x_object_processed]) / BigDecimal.new(results[:x_object_count])) * BigDecimal.new("100")).to_s("F")
+                    results[:batch] = output[:x_batch]
+                    if verbose_mode
+                        Gcore::Api.logger.puts(results)
+                    end
+                    break if results[:x_object_processed].to_i >= results[:x_object_count].to_i
+                rescue RestClient::ResourceNotFound
+                    #Carry on, loop again.
+                rescue StandardError => ex
+                    Gcore::Api.logger.puts({message: ex.message, class: ex.class.name})
+                end
+                sleep 1
+            end
+            
+            JSON.parse(RestClient.get("#{Gcore::Api.endpoint}#{output[:location]}", Gcore::Api.header), {symbolize_names: true})  
+        end
+        
+
         
       end
     end
